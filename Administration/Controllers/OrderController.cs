6cbf0d03fc20b37b2de2d13ae7dffb78456968e7
Mudasir <mudee124@gmail.com>
2021-06-10 18:59:38 +0530

    update3

---
+++ Administration/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Administration.Business_Layer;
+using Business_Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Administration.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetAll()
+        {
+            OrderManager obj = new OrderManager();
+            IEnumerable<_Order> list = obj.GetAllOrder();
+            return Json(new { msg = list, total = list.Count() }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Administration/Controllers/PlanController.cs
@@ -0,0 +1,26 @@
+﻿using Administration.Business_Layer;
+using Business_Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Administration.Controllers
+{
+    public class PlanController : Controller
+    {
+        // GET: Plan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetAll()
+        {
+            PlanManager obj = new PlanManager();
+            IEnumerable<Plan> list = obj.GetAllPlan();
+            return Json(new { msg = list, total = list.Count() }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
